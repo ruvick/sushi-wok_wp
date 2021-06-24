e1be6dd790c2dff989f2c0b357426f97a4e44011
Ruvick <rudikov.web@ya.ru>
2021-06-24 12:56:25 +0300

    Прописал стандартный single

--- single.php
+++ single.php
@@ -1,19 +1,30 @@
 <?php
 
-/*
-Template Name: Шаблон карточки товаров
-WP Post Template: Шаблон карточки товаров
-Template Post Type: post
-*/
-
 get_header(); ?>
 
 <?php get_template_part('template-parts/header-section');?>
 
-<main class="page">
+		<main class="page">
+			<div class="container">
 
+      <?php get_template_part('template-parts/sidebar-section');?>
 
+				<section class="main-page category">
 
-</main>
+				<?php
+			if ( function_exists('yoast_breadcrumb') ) {
+				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+			}
+			?> 
+				<div class="text_blk">
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<h1><?php the_title();?></h1>
+							<?php the_content();?>
+						<?php endwhile;?> 
+					<?php endif; ?>
+          </div>
+        </section>
+		</div>
+	</main>
 
 <?php get_footer();
