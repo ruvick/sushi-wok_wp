11a7fccd26accc4df67435973834f58e962843c9
Ruvick <rudikov.web@ya.ru>
2021-06-24 12:46:33 +0300

    Прописал стандартную страницу

--- page.php
+++ page.php
@@ -1,28 +1,15 @@
-
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package light_market
- */
-
-get_header(); ?>
+<?php get_header(); ?> 
 
 <?php get_template_part('template-parts/header-section');?>
 
-	<main id="primary" class="page site-main"> 
-
-		<section class="content"> 
+		<main class="page">
 			<div class="container">
 
-			<?php
+      <?php get_template_part('template-parts/sidebar-section');?>
+
+				<section class="main-page category">
+
+				<?php
 			if ( function_exists('yoast_breadcrumb') ) {
 				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
 			}
@@ -34,8 +21,8 @@ get_header(); ?>
 					<?php endwhile;?>
 				<?php endif; ?> 
 
-			</div>
-		</section>
+			</section>
+		</div>
 	</main>
 
 <?php get_footer();
