da82abc05da070174ad4728c6cc4ae154428e377
Ruvick <rudikov.web@ya.ru>
2021-06-24 13:12:55 +0300

    Прописал структуру таксономии

--- taxonomy-ultracat.php
+++ taxonomy-ultracat.php
@@ -1,18 +1,23 @@
-<?php get_header(); ?>
+<?php get_header(); ?> 
 
 <?php get_template_part('template-parts/header-section');?>
 
-<main class="page">
+		<main class="page">
+			<div class="container">
 
-	<section class="category">  
-		<div class="container">
+      <?php get_template_part('template-parts/sidebar-section');?>
 
-			<!-- Хлебные крошки -->
-			<?php
-			if ( function_exists('yoast_breadcrumb') ) {
-				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-			}
-			?> 
+				<section class="main-page category">
+
+					<div class="main-page__flex-block d-flex">
+						<p id="breadcrumbs"><span><span><a href="#">Главная</a> / <span><a href="#">Все
+											товары</a> / <span class="breadcrumb_last" aria-current="page">Суши и
+											ролы</span></span></span></span></p>
+						<a href="#" class="main-page__bascet d-flex">
+							<p class="main-page__bascet-link">Корзина</p>
+							<span class="main-page__bascet-number">1</span>
+						</a>
+					</div>
 
 			<!-- Вывод заголовка -->
 			<h1><?php single_cat_title( '', true );?></h1>
@@ -32,8 +37,9 @@
 			<!-- Пагинация -->
 			<?php if ( function_exists( 'wp_corenavi' ) ) wp_corenavi(); ?>
 
-		</div>
-	</section>
+
+			</section>
+	</div>
 </main>
 
 <?php get_footer(); ?>  
