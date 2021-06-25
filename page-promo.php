f5923ee0455c5e33945240673467ac48c65a23fe
Ruvick <rudikov.web@ya.ru>
2021-06-25 14:47:50 +0300

    Поправил корзину на стр Акции

--- page-promo.php
+++ page-promo.php
@@ -24,7 +24,7 @@ get_header(); ?>
 					?> 
 						<a href="#" class="main-page__bascet d-flex">
 							<p class="main-page__bascet-link">Корзина</p>
-							<span class="main-page__bascet-number">1</span>
+							<span class="main-page__bascet-number bascet_counter">0</span>
 						</a>
 					</div>
 
@@ -91,4 +91,4 @@ get_header(); ?>
 		</div>
 	</main>
 
-<?php get_footer();
+<?php get_footer(); 
