178f90133912c77da68d0c050adc1e02d82be8d2
asmi046 <asmi046@gmail.com>
2022-02-14 11:34:18 +0300

    Корзина фикс

--- js/bascet.js
+++ js/bascet.js
@@ -149,6 +149,9 @@ Vue.component('bascetform', {
 
             this.bascet = JSON.parse(localStorage.getItem("cart"));
 
+            this.bascetCount = localStorage.getItem("cartcount");
+            this.bascetSumm = localStorage.getItem("cartsumm"); 
+
             var params = new URLSearchParams();
             params.append('action', 'send_cart');
             params.append('nonce', allAjax.nonce);
